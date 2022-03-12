e19d0f6bcc08734592d4d3c1342d9565a469d0d0
Ali Parvizi <snparvizi75@gmail.com>
2022-03-12 01:15:18 -0800

    remove description/rules/tags from arena

--- contracts/Arena.sol
+++ contracts/Arena.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.4;
 
 contract Arena {
     string public name;             // arena name
-    string public description;      // overall description of the arena
-    string[] public rules;          // rules governing the arena
     address public token;                  // this is the token that is used to vote in this arena
     uint256 public minContributionAmount;  // minimum amount of voting/contributing
     bool public allowChoiceFunds;   // can choices receive funding from votes
@@ -12,35 +10,27 @@ contract Arena {
     uint public arenaFeePercentage; // percentage of each vote that goes to the arena
     uint256 public choiceCreationFee;  // to prevent spam choice creation
     uint256 public topicCreationFee;   // to prevent spam topic creation
-    string[] public tags;
 
     function info() public view returns(
-        string memory, string memory,
-        string[] memory, address, uint256,
-        bool, bool, uint, uint256, uint256,
-        string[] memory
+        string memory, address, uint256,
+        bool, bool, uint, uint256, uint256
     ) {
-        return (name, description, rules, token, minContributionAmount,
+        return (name,token, minContributionAmount,
                 allowTopicFunds, allowTopicFunds, arenaFeePercentage,
-        choiceCreationFee,topicCreationFee,tags);
+        choiceCreationFee,topicCreationFee);
     }
 
     constructor(
         string memory _name,
-        string memory _description,
-        string[] memory _rules,
         address _token,
         uint256 _minContribAmount,
         bool _allowChoiceFunds,
         bool _allowTopicFunds,
         uint _arenaFeePercentage,
         uint256 _choiceCreationFee,
-        uint256 _topicCreationFee,
-        string[] memory _tags
+        uint256 _topicCreationFee
     ) {
         name = _name;
-        description = _description;
-        rules = _rules;
         token = _token;
         minContributionAmount = _minContribAmount;
         allowChoiceFunds = _allowChoiceFunds;
@@ -48,6 +38,5 @@ contract Arena {
         arenaFeePercentage = _arenaFeePercentage;
         choiceCreationFee = _choiceCreationFee;
         topicCreationFee = _topicCreationFee;
-        tags = _tags;
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -2,47 +2,29 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 import { BigNumber } from "ethers";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+const arenaDeployParamsDict = {
+  name: "Test Arena",
+  token: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
+  minContributionAmount: BigNumber.from(10),
+  allowChoiceFunds: true,
+  allowTopicFunds: true,
+  arenaFeePercentage: BigNumber.from(10),
+  choiceCreationFee: BigNumber.from(10),
+  topicCreationFee: BigNumber.from(10),
+};
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+function getFlatParamsFromDict(paramsDict: any) {
+  return Object.entries(paramsDict).map((e) => e[1]);
+}
 
 describe("Attention Stream Setup", () => {
   it("Should create an arena", async () => {
     const Arena = await ethers.getContractFactory("Arena");
-    const deployParams = {
-      name: "Test Arena",
-      description: "Test Arena Description",
-      rules: ["r1", "r2", "r3"],
-      token: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
-      minContributionAmount: BigNumber.from(10),
-      allowChoiceFunds: true,
-      allowTopicFunds: true,
-      arenaFeePercentage: BigNumber.from(10),
-      choiceCreationFee: BigNumber.from(10),
-      topicCreationFee: BigNumber.from(10),
-      tags: ["t1", "t2"],
-    };
-
-    const params = Object.entries(deployParams).map((e) => e[1]);
-
+    const arenaDeployParams = getFlatParamsFromDict(arenaDeployParamsDict);
     // @ts-ignore
-    const arena = await Arena.deploy(...params);
+    const arena = await Arena.deploy(...arenaDeployParams);
     await arena.deployed();
     const info = await arena.functions.info();
-
-    expect(info).to.deep.include.members(params);
+    expect(info).to.deep.include.members(arenaDeployParams);
   });
 });
