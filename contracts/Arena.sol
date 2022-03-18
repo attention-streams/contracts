50132ed70342f0f6f45bd8f7970168f3acb0c32c
Ali Parvizi <snparvizi75@gmail.com>
2022-03-18 10:13:13 -0700

    reformat and comments

--- contracts/Arena.sol
+++ contracts/Arena.sol
@@ -2,22 +2,39 @@
 pragma solidity ^0.8.4;
 
 contract Arena {
-    string public name;             // arena name
-    address public token;                  // this is the token that is used to vote in this arena
-    uint256 public minContributionAmount;  // minimum amount of voting/contributing
-    bool public allowChoiceFunds;   // can choices receive funding from votes
-    bool public allowTopicFunds;    // can topics receive funding from votes
+    string public name; // arena name
+    address public token; // this is the token that is used to vote in this arena
+    uint256 public minContributionAmount; // minimum amount of voting/contributing
+    bool public allowChoiceFunds; // can choices receive funding from votes
+    bool public allowTopicFunds; // can topics receive funding from votes
     uint16 public arenaFeePercentage; // percentage of each vote that goes to the arena, decimal places: 2
-    uint256 public choiceCreationFee;  // to prevent spam choice creation
-    uint256 public topicCreationFee;   // to prevent spam topic creation
+    uint256 public choiceCreationFee; // to prevent spam choice creation
+    uint256 public topicCreationFee; // to prevent spam topic creation
 
-    function info() public view returns(
-        string memory, address, uint256,
-        bool, bool, uint16, uint256, uint256
-    ) {
-        return (name,token, minContributionAmount,
-                allowTopicFunds, allowTopicFunds, arenaFeePercentage,
-        choiceCreationFee,topicCreationFee);
+    function info()
+        public
+        view
+        returns (
+            string memory, // arena name
+            address, // voting token
+            uint256, // min contrib amount
+            bool, // allow choice funds
+            bool, // allow topic funds
+            uint16, // arena fee percentage
+            uint256, // choice creation fee
+            uint256 // topic creation fee
+        )
+    {
+        return (
+            name,
+            token,
+            minContributionAmount,
+            allowTopicFunds,
+            allowTopicFunds,
+            arenaFeePercentage,
+            choiceCreationFee,
+            topicCreationFee
+        );
     }
 
     constructor(
