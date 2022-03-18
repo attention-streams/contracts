d23f98e3f28cbd5b002c203c436b478cd41460bd
Ali Parvizi <snparvizi75@gmail.com>
2022-03-18 10:40:50 -0700

    use max choice/topic fee percentage instead of boolean

--- contracts/Arena.sol
+++ contracts/Arena.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.8.4;
 
 contract Arena {
     string public name; // arena name
+
     address public token; // this is the token that is used to vote in this arena
+
     uint256 public minContributionAmount; // minimum amount of voting/contributing
-    bool public allowChoiceFunds; // can choices receive funding from votes
-    bool public allowTopicFunds; // can topics receive funding from votes
-    uint16 public arenaFeePercentage; // percentage of each vote that goes to the arena, decimal places: 2
+
+    // all percentage fields assume 2 decimal places
+    uint16 public maxChoiceFeePercentage; // max percentage of a vote taken as fees for a choice
+    uint16 public maxTopicFeePercentage; // max percentage of a vote taken as fees for a topic
+    uint16 public arenaFeePercentage; // percentage of each vote that goes to the arena
+
     uint256 public choiceCreationFee; // to prevent spam choice creation
     uint256 public topicCreationFee; // to prevent spam topic creation
 
@@ -18,8 +23,8 @@ contract Arena {
             string memory, // arena name
             address, // voting token
             uint256, // min contrib amount
-            bool, // allow choice funds
-            bool, // allow topic funds
+            uint16, // max choice funds
+            uint16, // max topic funds
             uint16, // arena fee percentage
             uint256, // choice creation fee
             uint256 // topic creation fee
@@ -29,8 +34,8 @@ contract Arena {
             name,
             token,
             minContributionAmount,
-            allowTopicFunds,
-            allowTopicFunds,
+            maxChoiceFeePercentage,
+            maxTopicFeePercentage,
             arenaFeePercentage,
             choiceCreationFee,
             topicCreationFee
@@ -41,8 +46,8 @@ contract Arena {
         string memory _name,
         address _token,
         uint256 _minContribAmount,
-        bool _allowChoiceFunds,
-        bool _allowTopicFunds,
+        uint16 _maxChoiceFeePercentage,
+        uint16 _maxTopicFeePercentage,
         uint16 _arenaFeePercentage,
         uint256 _choiceCreationFee,
         uint256 _topicCreationFee
@@ -51,8 +56,8 @@ contract Arena {
         name = _name;
         token = _token;
         minContributionAmount = _minContribAmount;
-        allowChoiceFunds = _allowChoiceFunds;
-        allowTopicFunds = _allowTopicFunds;
+        maxChoiceFeePercentage = _maxChoiceFeePercentage;
+        maxTopicFeePercentage = _maxTopicFeePercentage;
         arenaFeePercentage = _arenaFeePercentage;
         choiceCreationFee = _choiceCreationFee;
         topicCreationFee = _topicCreationFee;
