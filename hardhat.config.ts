d698cb1f97d4998e7e1bf0871262634420580163
Ali Parvizi <snparvizi75@gmail.com>
2022-03-26 11:15:32 -0700

    gas reporter setting from env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -42,7 +42,7 @@ const config: HardhatUserConfig = {
     },
   },
   gasReporter: {
-    enabled: true,
+    enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
   },
   etherscan: {
