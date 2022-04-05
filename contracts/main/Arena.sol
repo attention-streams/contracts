a12ec2707d8351c171fa611e2334c158377819a4
Ali Parvizi <snparvizi75@gmail.com>
2022-04-05 02:56:44 -0700

    move PositionUtils into Position.sol

--- contracts/main/Arena.sol
+++ contracts/main/Arena.sol
@@ -21,37 +21,6 @@ struct ArenaInfo {
     address payable _funds; // arena funds location
 }
 
-library PositionUtils {
-    function updatePosition(
-        Position storage p,
-        Topic memory topic,
-        uint256 amount
-    ) internal {
-        // update user postion data
-        p.checkPointShares = getShares(p, topic);
-        p.tokens += amount;
-        p.blockNumber = block.number;
-    }
-
-    function getShares(Position memory p, Topic memory t)
-        internal
-        view
-        returns (uint256)
-    {
-        // cycles passed
-        uint256 shares;
-        uint256 cyclesPassed = (block.number - p.blockNumber) /
-            t._cycleDuration;
-        shares =
-            p.tokens *
-            cyclesPassed *
-            (t._sharePerCyclePercentage / 10000) +
-            p.checkPointShares;
-
-        return shares;
-    }
-}
-
 contract Arena {
     using PositionUtils for Position;
 
--- contracts/main/Position.sol
+++ contracts/main/Position.sol
@@ -1,4 +1,38 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+import "./Topic.sol";
+
+library PositionUtils {
+    function updatePosition(
+        Position storage p,
+        Topic memory topic,
+        uint256 amount
+    ) internal {
+        // update user postion data
+        p.checkPointShares = getShares(p, topic);
+        p.tokens += amount;
+        p.blockNumber = block.number;
+    }
+
+    function getShares(Position memory p, Topic memory t)
+        internal
+        view
+        returns (uint256)
+    {
+        // cycles passed
+        uint256 shares;
+        uint256 cyclesPassed = (block.number - p.blockNumber) /
+            t._cycleDuration;
+        shares =
+            p.tokens *
+            cyclesPassed *
+            (t._sharePerCyclePercentage / 10000) +
+            p.checkPointShares;
+
+        return shares;
+    }
+}
 
 struct Position {
     // share are dynamically calculated as follows
