d273a3e00134577335dd703d271d0b5a2852e828
Ali Parvizi <snparvizi75@gmail.com>
2023-08-28 09:35:35 +0200

    added OpenZeppelin dependencies

--- contracts/Choice.sol
+++ contracts/Choice.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "./interfaces/ITopic.sol";
 
