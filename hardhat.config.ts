85eb446ba14b318f43ec9605b175428ce517979f
adamstallard <205792+adamstallard@users.noreply.github.com>
2024-11-08 07:26:54 -0800

    add foundry

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-etherscan";
 import "@openzeppelin/hardhat-upgrades";
-
+import "@nomicfoundation/hardhat-foundry";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
