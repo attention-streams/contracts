a1c54486091bb3198b33346d2adc1ecf5189b892
adamstallard <205792+adamstallard@users.noreply.github.com>
2024-11-09 07:56:45 -0800

    functions for creating ideas solutions and profiles

--- contracts/Updraft.sol
+++ contracts/Updraft.sol
@@ -8,12 +8,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Updraft is Ownable {
     using SafeERC20 for IERC20;
 
-    /// @notice Allow up to 2 decimal places for percentages.
+    /// Allow 2 decimal places for percentages.
     /// Examples: 100% = 10000 , 25.34% = 2534
     uint256 public constant PERCENT_SCALE = 100;
 
     IERC20 public feeToken;
+
+    /// `minFee` is the minimum fee (in `feeToken`) paid for creating or contributing to an idea,
+    /// and the only fee paid for creating solutions and updating profiles.
     uint256 public minFee;
+
+    /// `percentFee` is the percentage used to calculate the feed paid for creating or contributing to an idea.
+    /// It's multiplied by the contribution amount and the fee paid is the greater of the result and `minFee`.
+    /// It uses `PERCENT_SCALE` for precision.
     uint256 public percentFee;
 
     event ProfileUpdated(address indexed owner, bytes32 data);
@@ -50,4 +57,32 @@ contract Updraft is Ownable {
     function setPercentFee(uint256 amount) external onlyOwner {
         percentFee = amount;
     }
+
+    /// Create or update a profile. It will be associated with the caller's address.
+    function updateProfile(bytes calldata profileData) external {
+        feeToken.safeTransferFrom(msg.sender, address(0), minFee);
+        emit ProfileUpdated(msg.sender, profileData);
+    }
+
+    function createIdea(uint256 contributorFee, uint256 contribution, bytes calldata ideaData) external {
+        uint256 fee = max(minFee, contribution * percentFee / PERCENT_SCALE);
+
+        feeToken.safeTransferFrom(msg.sender, address(0), fee);
+
+    }
+
+    function createSolution() external {
+        feeToken.safeTransferFrom(msg.sender, address(0), minFee);
+    }
+
+    /// Create or update a profile while creating an idea, to avoid paying the updraft anti-spam fee twice.
+    function createIdeaWithProfile(bytes calldata profileData) external {
+        emit ProfileUpdated(msg.sender, profileData);
+    }
+
+    /// Create or update a profile while creating a solution, to avoid paying `minFee` twice.
+    function createSolutionWithProfile(bytes calldata profileData) external {
+        feeToken.safeTransferFrom(msg.sender, address(0), minFee);
+    }
+
 }
