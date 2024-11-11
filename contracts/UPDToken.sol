fd17d2e356db98438fcd99f384b42f50d688c9f7
adamstallard <205792+adamstallard@users.noreply.github.com>
2024-11-10 21:37:46 -0800

    Rename token contract to UPDToken

--- contracts/UPDToken.sol
+++ contracts/UPDToken.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.22;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract Updraft is ERC20, ERC20Permit {
+contract UPDToken is ERC20, ERC20Permit {
     constructor() ERC20("Updraft", "UPD") ERC20Permit("Updraft") {
         _mint(msg.sender, 100000000000 * 10 ** decimals());
     }
